24de619f47cc66388cd486ce5d364171d6fafe99
ianpike <ianpike98@gmail.com>
2026-03-23 20:24:58 -0400

    Initial commit

---
+++ Rex.Server/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Rex.Server;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Rex.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Rex.Shared;
+
+public class Class1
+{
+}
