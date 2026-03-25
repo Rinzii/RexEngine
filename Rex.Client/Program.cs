e7322dee65cc4d4d2540f2c55bed34bc6e728930
ThePedal7 <137915224+ThePedal7@users.noreply.github.com>
2026-03-24 21:10:16 -0500

    Adds Prototype Windows Class

--- Rex.Client/Program.cs
+++ Rex.Client/Program.cs
@@ -1,23 +1,41 @@
-﻿using Silk.NET;
+﻿using System.Drawing;
+using Silk.NET;
 using Silk.NET.Core.Contexts;
 using Silk.NET.SDL;
-
 namespace Rex.Client;
 
-internal static unsafe class Program {
+internal static class Program {
+    
+    private static void Main(string[] args) {
+       Windows.InitSDL();
+       Windows.GameLoop();
+       
+       
+    }
+    
+}
+
+unsafe class App {
+    internal Renderer* renderer;
+    internal Window* window;
+    
+}
+
+internal unsafe class Windows {
+    
     private static readonly Sdl _sdl = Sdl.GetApi();
     private static App app = new App();
 
     private const int SCREEN_WIDTH = 800;
     private const int SCREEN_HEIGHT = 600;
 
-    static void InitSDL() {
+    public static void InitSDL() {
         int rendererFlags = (int)RendererFlags.Accelerated;
         int windowFlags = 0;
 
         if (_sdl.Init(Sdl.InitVideo) < 0) {
-            Console.WriteLine($"Couldn't initialize SDL: {_sdl.GetErrorS()}");
-            Environment.Exit(1);
+            Kill($"Couldn't initialize SDL: {_sdl.GetErrorS()}");
+            
         }
         app.window = _sdl.CreateWindow(
             "Hello, World",
@@ -27,36 +45,48 @@ internal static unsafe class Program {
             SCREEN_HEIGHT,
             (uint)windowFlags);
         if (app.window == null) {
-            Console.WriteLine($"Failed to open {SCREEN_WIDTH} x {SCREEN_HEIGHT} window: {_sdl.GetErrorS()}");
+            Kill($"Failed to open {SCREEN_WIDTH} x {SCREEN_HEIGHT} window: {_sdl.GetErrorS()}");
         }
 
         _sdl.SetHint(Sdl.HintRenderScaleQuality, "linear");
         app.renderer = _sdl.CreateRenderer(app.window, -1, (uint)rendererFlags);
 
         if (app.renderer == null) {
-            Console.WriteLine($"Failed to create renderer: {_sdl.GetErrorS()}");
-            Environment.Exit(1);
+            Kill($"Failed to create renderer: {_sdl.GetErrorS()}");
         }
     }
 
-    static void GameLoop() {
-        //TODO Later lol
+    static  void Draw() {
+        // ---- filled rectangle (red) ----
+        _sdl.SetRenderDrawColor(app.renderer, 255, 0, 0, 255);
+        FRect redBox = new FRect { X = 50, Y = 50, H = 300, W = 400 };
+        _sdl.RenderFillRectF(app.renderer, in redBox);
     }
-    private static void Main(string[] args) {
-       InitSDL();
-       while (Console.ReadLine() != "1") {
-           
-       }
-       
-       _sdl.DestroyRenderer(app.renderer);
-       _sdl.DestroyWindow(app.window);
-       _sdl.Quit();
-    }
-    
-}
 
-unsafe class App {
-    internal Renderer* renderer;
-    internal Window* window;
-    
+    static void Kill(string msg) {
+        Console.WriteLine(msg);
+        Environment.Exit(1);
+    }
+
+    public static void GameLoop() {
+        bool running = true;
+
+        Event e;
+        
+        while (running) {
+            while (_sdl.PollEvent(&e) != 0) {
+                if (e.Type == (uint)EventType.Quit) {
+                    running = false;
+                }
+            }
+            _sdl.SetRenderDrawColor(app.renderer, 30, 30, 30, 255);
+            _sdl.RenderClear(app.renderer);
+            
+            Draw();
+            _sdl.RenderPresent(app.renderer);
+        }
+        _sdl.DestroyRenderer(app.renderer);
+        _sdl.DestroyWindow(app.window);
+        _sdl.Quit();
+    }
 }
