33f56ecc3b195d869ab01e5345b910fb399a12d7
Ian Pike <Ianpike98@gmail.com>
2026-03-29 20:30:20 -0400

    Fix issues with window creation that is upsetting the static analyizer

--- Rex.Client/WindowCreator.cs
+++ Rex.Client/WindowCreator.cs
@@ -9,16 +9,31 @@ namespace Rex.Client;
 
 public sealed class WindowCreator : IGameWindow
 {
-    private IWindow? WINDOW { get; set; }
-    public string? Title { get; set; }
+    private IWindow? WindowHandle { get; set; }
+    public string Title { get; set; } = string.Empty;
     public int Width { get; private set; }
     public int Height { get; private set; }
     public bool IsOpen { get; private set; }
 
+    // TODO: Use these. They mirror what Silk.NET provides for Windowing.
+    // TODO: Remove all of these disable stuff once we are actually using them.
+#pragma warning disable CS0067 // Event is never used
+    // ReSharper disable EventNeverSubscribedTo.Local
+    // ReSharper disable EventNeverSubscribedTo.Global
+    public event Action<Vector2D<int>>? OnResize;
+    public event Action<Vector2D<int>>? OnFramebufferResize;
+    public event Action? OnClosing;
+    public event Action<bool>? OnFocusChanged;
+    public event Action? OnLoad;
+    public event Action<double>? OnUpdate;
+    public event Action<double>? OnRender;
+    // ReSharper restore EventNeverSubscribedTo.Global
+    // ReSharper restore EventNeverSubscribedTo.Local
+#pragma warning restore CS0067 // Event is never used
 
     public void Dispose()
     {
-        WINDOW.Dispose();
+        WindowHandle?.Dispose();
     }
     public void Open(string title, int width, int height)
     {
@@ -26,25 +41,24 @@ public sealed class WindowCreator : IGameWindow
         Width = width;
         Height = height;
 
-        WindowOptions options = WindowOptions.Default with
+        var options = WindowOptions.Default with
         {
             Size = new Vector2D<int>(Width, Height),
             // ReSharper disable once ArrangeThisQualifier
             Title = this.Title
         };
 
-        IWindow window = Window.Create(options);
-        WINDOW = window;
-        WINDOW.Load += OnLoad;
-        WINDOW.Update += OnUpdate;
-        WINDOW.Render += OnRender;
+        var window = Window.Create(options);
+        WindowHandle = window;
+        WindowHandle.Load += HandleLoad;
+        WindowHandle.Update += HandleUpdate;
+        WindowHandle.Render += HandleRender;
         window.Run();
         IsOpen = true;
     }
     public void Close()
     {
-
-        WINDOW.Close();
+        WindowHandle?.Close();
         IsOpen = false;
     }
 
@@ -58,21 +72,22 @@ public sealed class WindowCreator : IGameWindow
         throw new NotImplementedException();
     }
 
-    private void OnLoad()
+    private void HandleLoad()
     {
-        IInputContext input = WINDOW.CreateInput();
-        for (int i = 0; i < input.Keyboards.Count; i++)
+        var window = WindowHandle ?? throw new InvalidOperationException("Load fired before the window was assigned.");
+        var input = window.CreateInput();
+        foreach (var t in input.Keyboards)
         {
-            input.Keyboards[i].KeyDown += KeyDown;
+            t.KeyDown += KeyDown;
         }
     }
 
-    private void OnUpdate(double deltaTime)
+    private void HandleUpdate(double deltaTime)
     {
 
     }
 
-    private void OnRender(double deltaTime)
+    private void HandleRender(double deltaTime)
     {
 
     }
